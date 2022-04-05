b053e0761a7cabdc807d4e9c82030737dd5d7f35
Chavoloco <58275860+Chavoloco@users.noreply.github.com>
2022-04-05 12:24:41 -0300

    readConfig edited

--- src/main/scala/com/qa/util/ConfigUtil.scala
+++ src/main/scala/com/qa/util/ConfigUtil.scala
@@ -1,9 +1,15 @@
 package com.qa.util
 
+import com.qa.util.ReadConfig.configFile
 import com.typesafe.config.{Config, ConfigFactory}
 
 trait ConfigUtil {
 
     val configFile: Config = ConfigFactory.load("performance.conf")
+
+    val baseUrl = configFile.getString("configurations.baseUrl")
+    val durationMeasurements = configFile.getString("configurations.durationMeasurements")
+    val simulationDuration = configFile.getString("configurations.simulationDuration").toInt
+    val loadUsers = configFile.getString("configurations.loadUsers").toInt
 
 }
--- src/test/scala/com/qa/BaseSimulation.scala
+++ src/test/scala/com/qa/BaseSimulation.scala
@@ -1,37 +1,36 @@
 package com.qa
 
-import com.qa.util.ReadConfig
-import com.typesafe.config.ConfigUtil
+import com.qa.util.ConfigUtil
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 
 import scala.concurrent.duration.DurationInt
 
-class BaseSimulation extends Simulation{
+class BaseSimulation extends Simulation with ConfigUtil{
 
   val csvFeederLoginSuccessful = csv("data/loginSuccessful.csv").circular
   val csvFeederLoginWithFails = csv("data/loginFailed.csv").circular
 
-  val durationMeasurements = "seconds"
-  val simulationDuration = 2
-  val loadUsers = 10
+  //val durationMeasurements = "seconds"
+  //val simulationDuration = 2
+  //val loadUsers = 10
 
 
   val httpProtocol = http
-    .baseUrl(ReadConfig.baseUrl)
+    .baseUrl(baseUrl)
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
   def setInjectionProfile(scenario: ScenarioBuilder, scnName: String) = {
-    if (ReadConfig.durationMeasurements.equals("hours")) {
-      scenario.inject(rampUsers(ReadConfig.loadUsers * ReadConfig.simulationDuration) during ReadConfig.simulationDuration.hours)
-    } else if (ReadConfig.durationMeasurements.equals("minutes")) {
-      scenario.inject(rampUsers(ReadConfig.loadUsers) during ReadConfig.simulationDuration.minutes)
+    if (durationMeasurements.equals("hours")) {
+      scenario.inject(rampUsers(loadUsers * simulationDuration) during simulationDuration.hours)
+    } else if (durationMeasurements.equals("minutes")) {
+      scenario.inject(rampUsers(loadUsers) during simulationDuration.minutes)
     } else {
-      scenario.inject(rampUsers(ReadConfig.loadUsers) during ReadConfig.simulationDuration.seconds)
+      scenario.inject(rampUsers(loadUsers) during simulationDuration.seconds)
     }
   }
 
